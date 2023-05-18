c60f5f4ba42ffa865135758006f3fb607096454f
gabrielduarte <gmduarte96@gmail.com>
2023-05-18 17:28:18 -0300

    Feature: getting aggregated donations from firebase

--- vaquinhadori/lib/donationPartition.dart
+++ vaquinhadori/lib/donationPartition.dart
@@ -2,35 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:vaquinhadori/myTheme.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+class Transaction {
+  final num amount;
+  final String donatorName;
+  final num aggregatedDonations;
+  final num donatorCount;
+
+  Transaction(this.amount, this.donatorName, this.aggregatedDonations,
+      this.donatorCount);
+}
+
+class GetLastTransaction extends StatelessWidget {
+  const GetLastTransaction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<QuerySnapshot>(
+      future: FirebaseFirestore.instance
+          .collection('transactions')
+          .orderBy("donationDateTime", descending: true)
+          .limit(1)
+          .get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text("Erro ao carregar dados.");
+        }
+        if (!snapshot.hasData) {
+          return const Text("Erro ao carregar dados");
+        }
+        Map<String, dynamic> data =
+            snapshot.data?.docs.first.data() as Map<String, dynamic>;
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Text("Atingido: ${data["aggregatedDonations"]} ");
+          // return const LinearProgressIndicator(value: 50);
+        }
+
+        return const Text("Carregando informações...");
+      },
+    );
+  }
+}
 
 class GetUserName extends StatelessWidget {
   final String documentId;
 
-  GetUserName(this.documentId);
+  const GetUserName(this.documentId, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    CollectionReference test = FirebaseFirestore.instance.collection('testCollection');
+    CollectionReference test =
+        FirebaseFirestore.instance.collection('testCollection');
 
     return FutureBuilder<DocumentSnapshot>(
       future: test.doc(documentId).get(),
       builder:
           (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-
         if (snapshot.hasError) {
-          return Text("Something went wrong");
+          return const Text("Something went wrong");
         }
 
         if (snapshot.hasData && !snapshot.data!.exists) {
-          return Text("Document does not exist");
+          return const Text("Document does not exist");
         }
 
         if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
           return Text("Full Name: ${data['full_name']} ${data['last_name']}");
         }
 
-        return Text("loading");
+        return const Text("loading");
       },
     );
   }
@@ -45,8 +86,11 @@ class DonationPartition extends StatelessWidget {
       decoration: const BoxDecoration(
         color: MyTheme.almostWhite,
       ),
+      // child: Column(
+      //   children: <Widget>[GetUserName('testDocument')],
+      // ),
       child: Column(
-        children: <Widget>[GetUserName('testDocument')],
+        children: const <Widget>[GetLastTransaction()],
       ),
     );
   }
