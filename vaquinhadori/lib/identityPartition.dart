b2624eff92278abcfeb8098e8c46620a342d5d73
RicardoCLake <ricardodolago@gmail.com>
2023-05-26 22:47:24 -0300

    identity partition design

--- vaquinhadori/lib/identityPartition.dart
+++ vaquinhadori/lib/identityPartition.dart
@@ -7,22 +7,77 @@ class IdentityPartition extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 90, horizontal: 30),
+      padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 40),
       decoration: const BoxDecoration(
         color: MyTheme.almostWhite,
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Row(children: [
+          Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
             const Text(
               "Quem sou?",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
             ),
+            const Expanded(child: SizedBox(height: 1)),
             Image.asset(
               'assets/images/perfil.png',
-              height: 150,
+              height: 200,
             )
-          ])
+          ]),
+          const SizedBox(height: 30),
+          RichText(
+              textAlign: TextAlign.justify,
+              text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: const [
+                    TextSpan(text: "Um "),
+                    TextSpan(
+                        text: "alegre ",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    TextSpan(
+                        text:
+                            "estudante de engenharia elétrica da Escola Politécnica da USP que se interessa por telecomunicações e sonha desde pequeno em estudar na "),
+                    TextSpan(
+                        text: "França. ",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    TextSpan(text: "\n\n"),
+                    TextSpan(
+                        text:
+                            "Tenho levado bem a sério esses anos de Poli, mantenho uma "),
+                    TextSpan(
+                        text: "média 9,4 ",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    TextSpan(
+                        text:
+                            "e fiz várias atividades extracurriculares. Porém, por diferentes razões, ainda não consegui nenhuma bolsa de estudos e, por isso, preciso da "),
+                    TextSpan(
+                        text: "sua ajuda.",
+                        style: TextStyle(fontWeight: FontWeight.bold))
+                  ])),
+          const SizedBox(height: 70),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                    style: DefaultTextStyle.of(context).style,
+                    children: const [
+                      TextSpan(
+                          style: TextStyle(fontFamily: MyTheme.secundaryFont),
+                          children: [
+                            TextSpan(
+                                text:
+                                    "\"Tenho a ambição de ir para fora conhecer visões e tecnologias além daquelas encontradas aqui. Porém, carrego um "),
+                            TextSpan(
+                                text: "coração brasileiro ",
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            TextSpan(
+                                text:
+                                    " e que está determinado a voltar para trabalhar em terras tupiniquins.\" \n~ Ricardo Chiquetto do Lago")
+                          ])
+                    ])),
+          ),
         ],
       ),
     );
